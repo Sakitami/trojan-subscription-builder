556c20d977236d0b4bfbd9676499cccec2a0ecc9
Sakitami <Sakitam2373@gmail.com>
2020-03-31 16:27:37 +0800

    Multi-user build scenarios

---
+++ database.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+import base64
+import os
+
+
+## Load source file
+dir = os.getcwd()
+if os.path.isfile("password.txt") and os.path.isfile("url.txt"):
+    print("Found source file,trying to load it...")
+    ## Read dictionary file password.txt and url.txt
+    with open('password.txt','r') as password:
+    	passline = password.read().splitlines()
+    with open('url.txt','r', encoding='UTF-8') as url:
+    	urlline = url.read().splitlines()
+    with open('username.txt','r', encoding='UTF-8') as username:
+    	username = username.read().splitlines()
+    print("Loaded sorce file,creating original file...")
+    ## Create a new directory and original files
+    if os.path.isdir("subscription"):    
+        os.chdir("subscription")
+    else:
+        os.mkdir("subscription")
+        os.chdir("subscription")
+    n = 0
+    ## Determine whether to use html suffix
+    print("Do you want to use \".html\" ?", end='')
+    htmlkey = input("[Y/n]:")
+    if htmlkey == "Y":
+        sub_name = passline[n]+ '.html'
+    else:
+        sub_name = passline[n]
+    ## Create files
+    for i in range(0,len(passline)):
+        subfile = open(sub_name, "w+")
+        subfile.close
+        n += 1
+    print("Created,building subscription file...")
+else:
+    print("No password.txt or url.txt found,please check the directory.")
+    while True:
+        print('Press Enter to exit...')
+        exitkey = input()
+        if exitkey == "Y":
+            exit()
+        else:
+            exit()
+
+
+## Build subscription file
+r = -1
+print('-'*8)
+for i in range(0,len(passline)):
+    urlist = []
+    r += 1
+    num = 0
+    print("Creating " + passline[r] + "'s subscription file...", end='')
+    for j in range(len(urlline)):
+        trourl = "trojan://" + username[r] + ":" + passline[r] + "@" + urlline[num]
+        urlist.append(trourl)
+        num += 1
+    ## Use base64 encode and write into files
+    trojanlist = "\r\n".join(urlist)
+    encryption = str(base64.b64encode(trojanlist.encode()), encoding = "utf-8")
+    ## Check the if html suffix used
+    if htmlkey == "Y":
+        subfile = open(passline[r] + '.html',"a")
+    else:
+        subfile = open(passline[r] , "a")
+    subfile.write(encryption)
+    subfile.close()
+    print('Done')
+print('-'*8 + "\n" + 'All done, please check the subscription directory.')
+
+a = input(u"Do you want to upload those to server?[Y/n]:")
+if a == "Y":
+    os.chdir(dir)
+    os.system("ftp.py")
+
+while True:
+    print('Press Enter to exit...')
+    exitkey = input()
+    if exitkey == "Y":
+        exit()
+    else:
+        exit()
---
+++ ftp.py
@@ -0,0 +1,65 @@
+import configparser
+from ftplib import FTP
+import time
+import tarfile
+import os
+
+# Load FTp Mode
+conf = configparser.ConfigParser()
+conf.read("config.cfg")
+serverip = conf.get("FTP", "server") 
+port = conf.getint("FTP", "port") 
+username = conf.get("FTP", "user") 
+password = conf.get("FTP", "password")
+dirname = conf.get("FTP", "dir")
+list_file = os.listdir("subscription")
+print("Load config success! \n Connecting to the server...")
+
+# Connect to Server
+try:
+    ftp = FTP()
+    ftp.connect(serverip,port)
+    ftp.login(username,password)
+    bufsize = 1024
+    ftp.cwd("/" + dirname)
+    print("Connected to the server!")
+except:
+    print("Connect failed!")
+    exit()
+
+# Delete Files
+a = input(u"Do You want to Delete all the files?[n/YES]")
+if a == "YES":
+    failed1 = success1 = 0
+    del_files = ftp.nlst()
+    print(del_files)
+    for i in del_files:
+        try:
+            ftp.delete(i)
+            print(i + "....Delected")
+            success1 += 1
+        except:
+            print(i + "....Failed")
+            faild1 += 1
+    failed1 = str(failed1)
+    success1 = str(success1)
+    print("All Done,deleted " + success1 + " files, " + failed1 + " files not be deleted.")
+
+# Upload Files
+os.chdir("subscription")
+failed2 = success2 = 0
+for i in list_file:
+    try:
+        fp = open(i,'rb')
+        ftp.storbinary('STOR ' + i, fp,bufsize)
+        print(i + "....Uploaded")
+        success2 += 1
+    except:
+        print(i + "....Failed")
+        failed2 += 1
+    failed2 = str(failed2)
+    success2 = str(success2)
+    print("All Done,upload " + success2 + " files, " + failed2 + " files not be uploaded.")
+
+fp.close()
+ftp.quit()
