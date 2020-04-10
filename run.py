8aa1324ee391824e2a392d48426ad74ac3789057
Sakitami <sakitam2373@gmail.com>
2020-04-10 11:19:37 +0800

    Optimization code

--- run.py
+++ run.py
@@ -1,76 +1,19 @@
 #!/usr/bin/python3
-import base64
 import os
 
-
-## Load source file
-if os.path.isfile("password.txt") and os.path.isfile("url.txt"):
-    print("Found source file,trying to load it...")
-    ## Read dictionary file password.txt and url.txt
-    with open('password.txt','r') as password:
-    	passline = password.read().splitlines()
-    with open('url.txt','r', encoding='UTF-8') as url:
-    	urlline = url.read().splitlines()
-    print("Loaded sorce file,creating original file...")
-    ## Create a new directory and original files
-    if os.path.isdir("subscription"):    
-        os.chdir("subscription")
-    else:
-        os.mkdir("subscription")
-        os.chdir("subscription")
-    n = 0
-    ## Determine whether to use html suffix
-    print("Do you want to use .html ?", end='')
-    htmlkey = input("[Y/n]:")
-    if htmlkey == "Y":
-        sub_name = passline[n] + '.html'
-    else:
-        sub_name = passline[n]
-    ## Create files
-    for i in range(0,len(passline)):
-        subfile = open(sub_name, "w+")
-        subfile.close
-        n += 1
-    print("Created,building subscription file...")
-else:
-    print("No password.txt or url.txt found,please check the directory.")
-    while True:
-        print('Press Enter to exit...')
-        exitkey = input()
-        if exitkey == "Y":
-            exit()
-        else:
-            exit()
-
-
-## Build subscription file
-r = -1
-print('-'*8)
-for i in range(0,len(passline)):
-    urlist = []
-    r += 1
-    num = 0
-    print("Creating " + passline[r] + "'s subscription file...", end='')
-    for j in range(len(urlline)):
-        trourl = "trojan://" + passline[r] + "@" + urlline[num]
-        urlist.append(trourl)
-        num += 1
-    ## Use base64 encode and write into files
-    trojanlist = "\n".join(urlist)
-    encryption = str(base64.b64encode(trojanlist.encode()), encoding = "utf-8")
-    ## Check the if html suffix used
-    if htmlkey == "Y":
-        subfile = open(passline[r] + '.html',"a")
-    else:
-        subfile = open(passline[r], "a")
-    subfile.write(encryption)
-    subfile.close()
-    print('Done')
-print('-'*8 + "\r\n" + 'All done, please check the subscription directory.')
+dirname = os.getcwd()
+print('-'*8 + '\nWelcome to Trojan Subscription Builder\n\033[1;35mversion 0.0.2\033[0m\n' + '-'*8)
+print(' ')
+print('Which mode do you want to use?\n1.common\n2.Trojan-Panel(with mysql database)\n3.exit')
 while True:
-    print('Press Enter to exit...')
-    exitkey = input()
-    if exitkey == "Y":
+    a = input(u'Choose one:')
+    if a == "1":
+        os.chdir(dirname)
+        os.system("python common.py")
+    elif a == "2":
+        os.chdir(dirname)
+        os.system("python database.py")
+    elif a == "3":
         exit()
     else:
-        exit()
+        print("Invalid input, please try again")
