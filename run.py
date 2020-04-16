b8eea7848ad682de2ec23a94f7c9738282cb3353
Sakitami <sakitam2373@gmail.com>
2020-04-16 15:58:06 +0800

    Add logo

--- run.py
+++ run.py
@@ -3,7 +3,12 @@ import os
 
 dirname = os.getcwd()
 print('-'*8 + '\nWelcome to Trojan Subscription Builder\n\033[1;35mversion 0.0.3\033[0m\n' + '-'*8)
-print(' ')
+print('By')
+print(''' ____        _    _ _                  _ 
+/ ___|  __ _| | _(_) |_ __ _ _ __ ___ (_)
+\___ \ / _` | |/ / | __/ _` | '_ ` _ \| |
+ ___) | (_| |   <| | || (_| | | | | | | |
+|____/ \__,_|_|\_\_|\__\__,_|_| |_| |_|_|'''+'\n'+'-'*8)
 print('Which mode do you want to use?\n1.common\n2.Trojan-Panel(with mysql database)\n3.Add a user to mysql database\n4.exit')
 while True:
     a = input(u'Choose one:')
