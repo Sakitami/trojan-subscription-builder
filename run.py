ff44336f07d001c81ec7277e63353f51032cf07e
Sakitami <sakitam2373@gmail.com>
2020-04-27 22:37:44 +0800

    Optimize the display.

--- run.py
+++ run.py
@@ -2,15 +2,16 @@
 import os
 
 dirname = os.getcwd()
-print('-'*8 + '\nWelcome to Trojan Subscription Builder\n\033[1;35mversion 0.0.4\033[0m\n' + '-'*8)
-print('By')
-print(''' ____        _    _ _                  _ 
+while True:
+    os.system('clear')
+    print('-'*8 + '\nWelcome to Trojan Subscription Builder\n\033[1;35mversion 0.0.4-1\033[0m\n' + '-'*8)
+    print('By')
+    print(''' ____        _    _ _                  _ 
 / ___|  __ _| | _(_) |_ __ _ _ __ ___ (_)
 \___ \ / _` | |/ / | __/ _` | '_ ` _ \| |
  ___) | (_| |   <| | || (_| | | | | | | |
 |____/ \__,_|_|\_\_|\__\__,_|_| |_| |_|_|'''+'\n'+'-'*8)
-print('Which mode do you want to use?\n1.common\n2.Trojan-Panel(with mysql database)\n3.Add a user to mysql database\n4.clash\n5.exit')
-while True:
+    print('Which mode do you want to use?\n1.common\n2.Trojan-Panel(with mysql database)\n3.Add a user to mysql database\n4.clash\n5.exit')
     a = input(u'Choose one:')
     if a == "1":
         os.chdir(dirname)
